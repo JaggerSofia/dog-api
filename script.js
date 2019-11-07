85a9cb03b546b255252f19259cedb6cae9be7684
Victoria Moore <victoriamoore563@gmail.com>
2019-11-06 19:50:51 -0800

    update

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 'use strict';
 
 function getDogImage(dog) {
-    const url = `https://dog.ceo/api/breeds/image/random/${dog}`
-    fetch(url)
-        .then(response => {
-            if(response.ok) {
-                return response.json
-            }    
-        })
-        .then(response => displayResults(responseJson))
+  fetch(`https://dog.ceo/api/breeds/image/random/${dog}`)
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
 }
 
 function displayResults(responseJson) {
-    $('.results').html(
-        `<img src='${responseJson.message}' class='results-img>`
+    console.log(responseJson);
+    for(let i=0; i<responseJson.message.length; i++)
+    $('.results').append(
+      `<img src="${responseJson.message[i]}" class="results-image">`
     )
     $('.results').removeClass('hidden');
-}
+  }
 
 function watchForm() {
-    $('form').submit(event => {
-        event.preventDefault();
-        const dog=$('.number').val();
-        getDogImage(dog);
-    })
+  $('form').submit(event => {
+    event.preventDefault();
+    const dog=$('.number').val();
+    getDogImage(dog);
+  });
 }
 
 $(function() {
-    console.log('App loaded! Waiting for submit!');
-    watchForm();
-})
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
